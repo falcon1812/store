ad6a3314e650644606b26806a68cc0f521abdd8d
falcon1812 <thefalcon1812@hotmail.com>
2014-11-20 14:28:19 -0430

    Create banner.php

---
+++ docs/banner.php
@@ -0,0 +1,33 @@
+<div class="wrapper">
+		<ul id="sb-slider" class="sb-slider">
+			<li>
+				<img src="images/gta.png" alt="image1"/>
+				<div class="sb-description">
+					<h3>GTA V</h3>
+				</div>
+			</li>
+			<li>
+				<img src="images/gears.png" alt="image2"/>
+				<div class="sb-description">
+					<h3>Gears of war</h3>
+				</div>
+			</li>
+			<li>
+				<img src="images/halo.png" alt="image1"/>
+				<div class="sb-description">
+					<h3>Halo 4</h3>
+				</div>
+			</li>
+			<li>
+				<img src="images/ac1.png" alt="image1"/>
+				<div class="sb-description">
+					<h3>Assassins creed</h3>
+				</div>
+			</li>
+		</ul>
+		<div id="shadow" class="shadow"></div>
+			<div id="nav-arrows" class="nav-arrows">
+				<a href="#">Next</a>
+				<a href="#">Previous</a>
+			</div>
+</div>

7aebfb2fa7992ef1de31d9500d55d59293f72f42
falcon1812 <thefalcon1812@hotmail.com>
2014-11-20 14:44:17 -0430

    Create toolbari2.php

---
+++ docs/toolbari2.php
@@ -0,0 +1,26 @@
+
+<center>
+	<ul class="toolbar">
+		<li><a href="index.php">Inicio</a></li>
+			<li><a href="#">Productos</a>
+				<ul>
+					<?php 
+						include "conexion.php";
+						$consult = "SELECT * FROM categoria";
+						$result = mysql_query($consult);
+						while ($row=mysql_fetch_array($result)) {
+						$nombre_categoria= $row['nombre'];	
+						$id= $row['id_categoria'];
+					?>
+					<li><a href="docs/catalogo.php?id=<?php echo $id; ?>"><?php echo $nombre_categoria; ?></a></li>
+					<?php } ?>
+				</ul>
+			</li>
+		<li>
+			<a href="docs/perfil.php">Perfil</a>
+		</li>
+		<li>
+			<a href="docs/logout.php">Salir</a>
+		</li>
+	</ul>
+</center>

8f398613a25d1fbe11d5aac0c68d0b9d2d67a229
falcon1812 <thefalcon1812@hotmail.com>
2014-11-20 14:37:53 -0430

    Create head.php

---
+++ docs/head.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>Cocoa</title>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+	<link rel="stylesheet" type="text/css" href="../css/style.css">
+	<link rel="shortcut icon" href="../img/cocoa.ico" />
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,500' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="../css/shadowbox.css">
+	<link rel="stylesheet" type="text/css" href="../css/jquery.dataTables.css">
+	<link rel="stylesheet" type="text/css" href="../css/jquery.dataTables_themeroller.css">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="../css/bootstrap-theme.css">
+	<script type="text/javascript" language="javascript" src="../js/jquery-2.1.1.min.js"></script>
+	<script type="text/javascript" src="../js/shadowbox.js"></script>
+	<script type="text/javascript" src="../js/jquery.dataTables.js"></script>
+	<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-53ed47db4b0b9d99" async></script>
+</head>

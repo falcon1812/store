647835ed8c675f390061d98337b699c5bb931e45
falcon1812 <thefalcon1812@hotmail.com>
2014-11-20 14:43:50 -0430

    Create toolbaradmin.php

---
+++ docs/toolbaradmin.php
@@ -0,0 +1,32 @@
+
+<center>
+	<ul class="toolbar">
+			<li><a href="#">Usuarios</a>
+				<ul>
+					<li><a href="addusuario.php">Añadir</a></li>
+					<li><a href="delete.php">Eliminar</a></li>
+					<li><a href="consultageneralu.php">Consulta</a></li>
+					<li><a href="modificar.php">Modificar</a></li>
+				</ul>
+			</li>
+				<li><a href="#">Productos</a>
+				<ul>
+					<li><a href="addproducto.php">Añadir</a></li>
+					<li><a href="deleteproducto.php">Eliminar</a></li>
+					<li><a href="consultageneralp.php">Consulta</a></li>
+					<li><a href="modificarproducto.php">Modificar</a></li>
+				</ul>
+			</li>
+				<li><a href="#">Categorias</a>
+				<ul>
+					<li><a href="addcategoria.php">Añadir</a></li>
+					<li><a href="deletecategoria.php">Eliminar</a></li>
+					<li><a href="consultageneralc.php">Consulta</a></li>
+					<li><a href="modificarcategoria.php">Modificar</a></li>
+				</ul>
+			</li>
+		<li>
+			<a href="logout.php">Salir</a>
+		</li>
+	</ul>
+</center>

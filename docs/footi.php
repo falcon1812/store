eeb9bb1bfae63bc4292a1300d60dd42bb3d78e5e
falcon1812 <thefalcon1812@hotmail.com>
2014-11-20 14:35:54 -0430

    Create footi.php

---
+++ docs/footi.php
@@ -0,0 +1,16 @@
+	
+	<footer>
+		<div id="subfooter">
+			<iframe src="https://www.google.com/maps/embed?pb=!1m22!1m12!1m3!1d3923.084497297456!2d-66.87482740922421!3d10.494005148100333!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m7!1i0!3e6!4m0!4m3!3m2!1d10.4935188!2d-66.8749077!5e0!3m2!1ses!2sve!4v1408042998811" width="300" height="180" frameborder="0" style="border:0;"></iframe>
+		</div>
+		<div id="subfooter">
+			<img src="images/phone.png" alt="telefono" /><span class="local">04169821644</span><br>
+			<img src="images/mail.png" alt="correo" /><span class="local">thefalcon1812@hotmail.com</span><br>
+			<img src="images/local.png" alt="local" /><span class="local">Calle Los Apamates CC Los Apamates</span><br>
+		</div>
+		<div id="subfooter">
+			<span><br>© 2014 esto fue desarrollado y elaborado por <a href="https://twitter.com/falcon1812">christian falcon</a><br>Todos los Derechos reservados<br> Copyright © 2014. All rights reserved.</span>
+		</div>
+	</footer>
+</body>
+</html>
